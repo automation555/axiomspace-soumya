eb12d6e7cb18a45cd22cd0524278f6ce3cb65d1d
rajnikant <59799061+automation555@users.noreply.github.com>
2021-10-27 12:51:34 +0530

    vulnerability check

--- 2.cpp
+++ 2.cpp
@@ -9,3 +9,24 @@ void fun () {
    
    fclose(fp);
 }
+
+void iterateFoo()
+{
+unsigned num;
+int i;
+scanf("%u",&num);
+for (i = 0; i < num; i++){
+foo();
+}
+}
+
+void iterateFo1o()
+{
+unsigned num;
+int i;
+scanf("%u",&num);
+if (num > 20) return;
+for (i = 0; i < num; i++){
+foo();
+}
+}
