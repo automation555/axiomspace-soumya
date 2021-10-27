11832ec89967a95da964b1f66099cb76dea52fcf
rajnikant <59799061+automation555@users.noreply.github.com>
2021-10-27 15:31:22 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+ void foobar(int x)
+  {
+      int local_array[7];
+      local_array[x] = 0;    // x is not bounds-checked
+  }
+void foobar1(int x)
+  {
+      int local_array[7];
+      // verify the parameter is in range
+      if (x >= 0 && x < 7)
+      {
+          local_array[x] = 0;
+      }
+  }  
+
+int main() {
+      foobar(15);
+      foobar1(15);
+      return 0;
+  }
+
 void fun () {
    FILE * fp;
 
